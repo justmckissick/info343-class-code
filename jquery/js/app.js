bfe2b7699690a6739f6106ec37ef3b209e0cc4e8
Justin McKissick <justmck@uw.edu>
2015-10-15 17:12:54 -0700

    This is some maps

--- jquery/js/app.js
+++ jquery/js/app.js
@@ -6,3 +6,20 @@
     http://api.openweathermap.org/data/2.5/weather?zip=98195,us&units=imperial&appid=bd82977b86bf27fb59a04b61b657fb6f
 */
 
+$(function() {
+    'use strict';
+    $('a').attr('target', '_blank');
+    $('article').hide().fadeIn(1000);
+    $('#toggle-article').click(function() {
+        $('article').fadeToggle();
+    });
+
+    var url=('http://api.openweathermap.org/data/2.5/weather?zip=98195,us&units=imperial&appid=bd82977b86bf27fb59a04b61b657fb6f');
+    $.getJSON(url).then(function(data) {
+            console.log(data);
+            var temperature = data.main.temp;
+            $('#temp').text(Math.round(temperature));
+    });
+
+    console.log('test');
+});
