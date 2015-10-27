095523f5b3d4082cfb7582c6f237f1763760b73d
Justin McKissick <justmck@uw.edu>
2015-10-27 15:30:19 -0700

    I didn't push it?

--- parse/js/app.js
+++ parse/js/app.js
@@ -2,8 +2,8 @@
     script for the index.html file
 */
 
-/* MINE Parse.initialize("EoQ6gXfjCy3uFflFdiQOr2vQLUeCyyHx7EPXjPTh", "e2MVQvVmkqmSSnPT92iKRR8bjQ6TR7iUOhsc1e2K"); */
-Parse.initialize("HwGkNK09YRPy3ZajicPwpZMfX9vqCyc4ghFl2eh7", "14BQF3zAPvaOR1sh6aEzXX5Wk1LTnBFQopjr1Rbj");
+Parse.initialize("EoQ6gXfjCy3uFflFdiQOr2vQLUeCyyHx7EPXjPTh", "e2MVQvVmkqmSSnPT92iKRR8bjQ6TR7iUOhsc1e2K");
+/* Parse.initialize("HwGkNK09YRPy3ZajicPwpZMfX9vqCyc4ghFl2eh7", "14BQF3zAPvaOR1sh6aEzXX5Wk1LTnBFQopjr1Rbj"); */
 $(function() {
     'use strict';
 
@@ -12,6 +12,7 @@ $(function() {
     //new query that will return all tasks ordered by createAt
     var tasksQuery = new Parse.Query(Task);
     tasksQuery.ascending('createdAt');
+    tasksQuery.notEqualTo('done', true);
 
     //reference to the task list element
     var tasksList = $('#tasks-list');
@@ -21,6 +22,9 @@ $(function() {
 
     //current set of tasks
     var tasks = [];
+    
+    //Reference to our rating element
+    var ratingElem = $('#rating');
 
     function displayError(err) {
         errorMessage.text(err.message);
@@ -32,11 +36,11 @@ $(function() {
     }
 
     function showSpinner() {
-        $('fa.spin').show();
+        $('.fa-spin').show();
     }
 
     function hideSpinner() {
-        $('fa.spin').hide();
+        $('.fa-spin').hide();
     }
 
     function fetchTasks() {
@@ -54,11 +58,28 @@ $(function() {
     function renderTasks() {
         tasksList.empty();
         tasks.forEach(function(task) {
-            $(document.createElement('li'))
+            var li = $(document.createElement('li'))
                 .text(task.get('title'))
-                .appendTo(tasksList);
+                .addClass(task.get('done') ? 'completed-task' : '')
+                .appendTo(tasksList)
+                .click(function() {
+                    task.set('done', !task.get('done'));
+                    task.save().then(renderTasks, displayError);
+                });
+            $(document.createElement('span'))
+                .raty({readOnly: true, 
+                    score: (task.get('rating') || 0),
+                    hints: ['crap', 'awful', 'ok', 'nice', 'awesome']})    
+                .appendTo(li);
         });
     }
+    
+    function showMessage(message) {
+        message = message || '1';
+        alert(message);
+    }
+    
+    // just for fun showMessage('World');
     
     //when the user submits the new task form...
     $('#new-task-form').submit(function(evt) {
@@ -68,8 +89,10 @@ $(function() {
         var title = titleInput.val();
         var task = new Task();
         task.set('title', title);
+        task.set('rating', ratingElem.raty('score'));
         task.save().then(fetchTasks, displayError).then(function() {
             titleInput.val('');
+            ratingElem.raty('set', true);
         });
         
         return false;
@@ -77,6 +100,8 @@ $(function() {
 
     //go and fetch tasks from the server
     fetchTasks();
+    
+    ratingElem.raty();
 
     window.setInterval(fetchTasks, 3000)
 });
