ba5fc0eb1fb1411747c41de08cb954daf0680642
Justin McKissick <justmck@uw.edu>
2015-11-03 15:29:28 -0800

    Getting there

--- angular/js/movies.js
+++ angular/js/movies.js
@@ -1,8 +1,36 @@
 
 angular.module('Movies', [])
     .controller('MoviesController', function($scope, $http) {
+        var ratingsMap = {
+            'Not Rated': 0,
+            'G': 1,
+            'PG': 2,
+            'PG-13': 3,
+            'R': 4,
+            'NC-17': 5,
+            'X': 6
+        };
+    
+        //Get our movies from the JSON file
         $http.get('data/movies-2014.min.json')
             .then(function(results) {
-                $scope.movies = results.data;
+                //results is an object with info about the entire HTTP response
+                //the data itself can be accessed via the 'data' property
+                $scope.movies = results.data.map(function(movie) {
+                    movie.ratingOrdinal = ratingsMap[movie.rating];
+                    return movie;    
+                });
+            
+                $scope.distributors = _.uniq(_.pluck($scope.movies, 'distributor'));    
+                
             });
+        $scope.setSort = function(propertyName) {
+            if ($scope.sortCol === propertyName) {
+                $scope.sortReverse = !$scope.sortReverse;    
+            }
+            else {
+                $scope.sortCol = propertyName;
+                $scope.sortReverse = false;
+            }
+        }
     });
