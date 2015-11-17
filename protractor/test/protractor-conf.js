358756133a6fdb658952b725281e47287ca5b099
Justin McKissick <justmck@uw.edu>
2015-11-17 15:31:53 -0800

    changes from protractor

--- protractor/test/protractor-conf.js
+++ protractor/test/protractor-conf.js
@@ -1,5 +1,5 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['*-spec.js'],
-    rootElement: 'body'
+    rootElement: 'body' //looks for ng-app attribute; if you don't put ng-app on body, then you need to move it "e.g. 'main'
 };
